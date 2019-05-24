7fe87c76ec40d6575d0521c12bf44880b3c4068a
DengYangWu <1585485925@qq.com>
2019-05-24 10:45:19 +0800

    5/24

--- Twitter/src/main/java/com/twitter/mapper/AdminsDao.java
+++
@@ -1,7 +0,0 @@
-package com.twitter.mapper;
-
-import com.twitter.pojo.Admins;
-
-public interface AdminsDao {
-    int insertAll(Admins admins);
-}
--- Twitter/src/main/java/com/twitter/mapper/LikesDao.java
+++
@@ -1,4 +0,0 @@
-package com.twitter.mapper;
-
-public interface LikesDao {
-}
