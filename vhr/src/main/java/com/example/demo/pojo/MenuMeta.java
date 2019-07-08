d2167670a4540bead1cb1eadfe33d1a397a729bc
DengYangWu <1585485925@qq.com>
2019-07-08 22:03:26 +0800

    7/8

---
+++ vhr/src/main/java/com/example/demo/pojo/MenuMeta.java
@@ -0,0 +1,26 @@
+package com.example.demo.pojo;
+
+import java.io.Serializable;
+
+//菜单元
+public class MenuMeta implements Serializable {
+
+    private boolean keepAlive;
+    private boolean requireAuth;
+
+    public boolean isKeepAlive() {
+        return keepAlive;
+    }
+
+    public void setKeepAlive(boolean keepAlive) {
+        this.keepAlive = keepAlive;
+    }
+
+    public boolean isRequireAuth() {
+        return requireAuth;
+    }
+
+    public void setRequireAuth(boolean requireAuth) {
+        this.requireAuth = requireAuth;
+    }
+}
