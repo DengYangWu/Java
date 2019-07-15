40ce9720e456dc8a9e67ad4391e67b33b8cb58f7
DengYangWu <1585485925@qq.com>
2019-07-15 18:32:03 +0800

    7/15

--- vhr/vhr/src/main.js
+++ vhr/vhr/src/main.js
@@ -1,17 +1,56 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-Vue.use(ElementUI)
-Vue.config.productionTip = false
+import store from './store'
+import {getRequest} from './utils/api'
+import {postRequest} from './utils/api'
+import {deleteRequest} from './utils/api'
+import {putRequest} from './utils/api'
+// import {initMenu} from './utils/utils'
+// import {isNotNullORBlank} from './utils/utils'
+// import './utils/filter_utils'
+// import 'font-awesome/css/font-awesome.min.css'
 
+
+Vue.config.productionTip = false
+Vue.use(ElementUI)
+Vue.use(Vuex)
+
+Vue.prototype.getRequest = getRequest;
+Vue.prototype.postRequest = postRequest;
+Vue.prototype.deleteRequest = deleteRequest;
+Vue.prototype.putRequest = putRequest;
+
+router.beforeEach((to, from, next)=> {
+    if (to.name == 'Login') {
+      next();
+      return;
+    }
+    var name = store.state.user.name;
+    if (name == '未登录') {
+      if (to.meta.requireAuth || to.name == null) {
+        next({path: '/', query: {redirect: to.path}})
+      } else {
+        next();
+      }
+    } else {
+      //initMenu(router, store);
+      if(to.path=='/chat')
+        store.commit("updateMsgList", []);
+      next();
+    }
+  }
+)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
